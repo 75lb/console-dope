bee39d5b46f2b84d0cc673dbb6b19b82d0a5b7c8
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-02-12 10:45:41 +0100

    basics done.. still wip

--- console.js
+++ console.js
@@ -14,106 +14,66 @@ var ansi = {
     bold: 1,
     underline: 4
 };
-
-var log = console.log;
-
-var formatFlags = [];
-
-Object.defineProperty(console, "red", { 
-    enumarable: true, 
-    get: function(){
-        formatFlags.push("red");
-        return console;
-    }
-});
-
-Object.defineProperty(console, "green", { 
-    enumarable: true, 
-    get: function(){
-        formatFlags.push("green");
-        return console;
-    }
-});
-
-Object.defineProperty(console, "underline", { 
-    enumarable: true, 
-    get: function(){
-        formatFlags.push("underline");
-        return console;
-    }
-});
-
-Object.defineProperty(console, "bold", { 
-    enumarable: true, 
-    get: function(){
-        formatFlags.push("bold");
-        return console;
-    }
-});
-
 var ansiFormat = "\x1b[%sm%s\x1b[0m";
 
-console.log = function(txt){
+var log = console.log,
+    err = console.error,
+    formatFlags = [],
+    out = console.writeStream || process.stdout;
+
+var format = util.format;
+
+util.format = function(){
+    var output = format.apply(null, arguments);
+    output = output.replace(/%u{(.*?)}/g, format(ansiFormat, ansi.underline, "$1"));
+    // process.stdout.write(output + "\n");
+    return output;
+};
+
+function addFormatProperty(format){
+    Object.defineProperty(console, format, { 
+        enumerable: true, 
+        get: function(){
+            formatFlags.push(format);
+            return console;
+        }
+    });
+}
+Object.keys(ansi).forEach(addFormatProperty);
+
+
+console.log = function(){
     var args = wodge.array(arguments),
         codes = formatFlags.map(function(format){ return ansi[format]; });
-    log(util.format(ansiFormat, codes.join(";"), txt));
+        
+    out.write(util.format("\x1b[%sm", codes.join(";")));
+    log.apply(this, arguments);
+    out.write("\x1b[0m");
+    formatFlags = [];
+    return console;
+};
+
+console.error = function(){
+    var args = wodge.array(arguments),
+        codes = formatFlags.map(function(format){ return ansi[format]; });
+        
+    process.stderr.write(util.format("\x1b[%sm", codes.join(";")));
+    err.apply(this, arguments);
+    process.stderr.write("\x1b[0m");
     formatFlags = [];
     return console;
 };
 
 console.write = function(txt){
     var codes = formatFlags.map(function(format){ return ansi[format]; });
-    process.stdout.write(util.format(ansiFormat, codes.join(";"), txt));
+    out.write(util.format(ansiFormat, codes.join(";"), txt));
     formatFlags = [];
     return console;
 };
 
-
-
-// exports.black = function(txt){
-//     return util.format(ansiFormat, ansi.black, txt);
-// };
-// exports.green = function(txt){
-//     return util.format(ansiFormat, ansi.green, txt);
-// };
-// exports.yellow = function(txt){
-//     return util.format(ansiFormat, ansi.yellow, txt);
-// };
-// exports.blue = function(txt){
-//     return util.format(ansiFormat, ansi.blue, txt);
-// };
-// exports.magenta = function(txt){
-//     return util.format(ansiFormat, ansi.magenta, txt);
-// };
-// exports.cyan = function(txt){
-//     return util.format(ansiFormat, ansi.cyan, txt);
-// };
-// exports.white = function(txt){
-//     return util.format(ansiFormat, ansi.white, txt);
-// };
-// exports.bold = function(txt){
-//     return util.format(ansiFormat, ansi.bold, txt);
-// };
-// exports.underline = function(txt){
-//     return util.format(ansiFormat, ansi.underline, txt);
-// };
-
-// exports.red = function(txt){
-//     return util.format(ansiFormat, ansi.red, txt);
-// };
-
-/**
-Add multiple ansi formats
-@method ansi
-@example
-    ansi("hello", "bold", "underline");
-    ansi("success", "green", "bold");
-*/
-exports.ansi = function(){
-    var args = wodge.array(arguments),
-        txt = args.shift(),
-        codes = args.map(function(arg){ return ansi[arg]; });
-    return util.format(ansiFormat, codes.join(";"), txt);
+console.column = function(col){
+    out.write(util.format("\x1b[%dG", col));
+    return console;
 };
 
 exports = console;
